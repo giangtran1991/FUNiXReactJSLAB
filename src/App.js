8d3aa5d33100a688e69240ab5f3ee6722fe1e959
Giang Tran <giangtran.buh@gmail.com>
2022-05-28 22:22:56 +0700

    LAB02_1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 class App extends Component {
-
-  render() {
-    return (
-      <div className="App">
-        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
-        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello! This is LAB02_1</h1>
+                <p>
+                    Sample cho toàn bộ lab và project trong môn học Lập Trình
+                    Web Front-End với React với đầy đủ dependency.
+                </p>
+                <p>
+                    Phiên bản React hiện tại trên máy của bạn là:{" "}
+                    {React.version}
+                </p>
+                <p>
+                    Phiên bản React đã được sử dụng để thiết kế môn học:
+                    16.14.0.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
