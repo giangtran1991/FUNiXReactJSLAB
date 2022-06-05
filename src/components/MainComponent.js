eefb077744abcb218d3a98ce7966f1852eaa3b9e
Giang Tran <giangtran.buh@gmail.com>
2022-06-05 15:14:10 +0700

    This is LAB05_3

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,10 +4,14 @@ import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
 import DishDetail from "./DishDetailComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 
 class Main extends Component {
     constructor(props) {
@@ -15,12 +19,33 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS,
         };
     }
 
     render() {
         const HomePage = () => {
-            return <Home />;
+            return (
+                <Home
+                    dish={
+                        this.state.dishes.filter(
+                            (dish) => dish.featured === true
+                        )[0]
+                    }
+                    promotion={
+                        this.state.promotions.filter(
+                            (promo) => promo.featured
+                        )[0]
+                    }
+                    leader={
+                        this.state.leaders.filter(
+                            (leader) => leader.featured
+                        )[0]
+                    }
+                />
+            );
         };
         return (
             <div>
@@ -32,6 +57,7 @@ class Main extends Component {
                         path="/menu"
                         component={() => <Menu dishes={this.state.dishes} />}
                     />
+                    <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
