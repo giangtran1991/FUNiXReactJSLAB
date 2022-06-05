9c094d30ffad7e62e2a3c783572412e403191832
Giang Tran <giangtran.buh@gmail.com>
2022-06-05 09:02:12 +0700

    This is LAB05_1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,10 @@
 // This is Container Component
 
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishDetailComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 import { DISHES } from "../shared/dishes";
 
 class Main extends Component {
@@ -21,13 +22,7 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <Navbar dark color="primary">
-                    <div className="container">
-                        <NavbarBrand href="/">
-                            Ristorante Con Fusion
-                        </NavbarBrand>
-                    </div>
-                </Navbar>
+                <Header />
                 <Menu
                     dishes={this.state.dishes}
                     onClick={(dishId) => this.onDishSelect(dishId)}
@@ -39,6 +34,7 @@ class Main extends Component {
                         )[0]
                     }
                 />
+                <Footer />
             </div>
         );
     }
